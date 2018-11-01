d6399be74926323e0f370a3de6e894a56fa76ba6
xuxiaoyan <xuxiaoyan@didichuxing.com>
2018-11-01 15:24:55 +0800

    fix:responsive initialize synchronization

--- src/assets/responsive.js
+++ src/assets/responsive.js
@@ -1,26 +1,17 @@
 (function (window, document) {
-
-  function resize(){
-    var ww = window.innerWidth;
+  function resize () {
+    var ww = window.innerWidth
     if (ww > window.screen.width) {
       window.requestAnimationFrame(resize)
-    }
-    else{
+    } else {
       if (ww > 720) {
         ww = 720
       }
       document.documentElement.style.fontSize = ww * 0.13333333333333333 + 'px'
-      document.body.style.opacity = 1
     }
   }
 
-  if (document.readyState !== 'loading') {
-    resize()
-  }
-  else {
-    document.addEventListener('DOMContentLoaded', resize)
-  }
+  resize()
 
   window.addEventListener('resize', resize)
-
 })(window, document)
